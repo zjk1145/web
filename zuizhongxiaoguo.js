cf4fbd84fe4ced6b54cb5642f6dfc711796d3b34
zjk1145 <3153953806@qq.com>
2024-11-12 19:35:33 +0800

    提交项目

---
+++ zuizhongxiaoguo.js
@@ -0,0 +1,48 @@
+//鼠标悬停
+window.onload = function(){
+    var image = document.getElementsByClassName('image2');
+    for(var i = 0; i < image.length;i ++){
+        image[i].style.cursor='pointer'
+    }
+}
+
+//最新消息
+function jumplink1() {
+    window.location.href = './最新消息/5.html';
+
+}
+function jumplink2() {
+    window.location.href = './最新消息/6.html'
+
+}
+function jumplink3() {
+    window.location.href = './最新消息/7.html'
+
+}
+var texts = document.getElementsByClassName('text');
+var links = ['./最新消息/2.html', './最新消息/3.html', './最新消息/4.html', './最新消息/5.html', './最新消息/6.html', './最新消息/7.html'];
+for (var i = 0; i < texts.length; i++) {
+    texts[i].style.color = '#337AB7';
+
+    texts[i].addEventListener('mouseover', function () {
+        this.href = links[Array.prototype.indexOf.call(texts, this)];
+        this.style.color = '#23537D';
+        this.style.textDecoration = 'underline';
+        this.style.cursor = 'pointer';
+    });
+
+    texts[i].addEventListener('mouseout', function () {
+        this.href = '';
+        this.style.color = '#337AB7';
+        this.style.textDecoration = 'none';
+        this.style.cursor = 'default';
+    });
+
+    texts[i].addEventListener('click', function (e) {
+        this.style.color = '#058A7D';
+        this.style.cursor = 'pointer';
+        if (this.href) {
+            window.location.href = this.href;
+        }
+    });
+}
